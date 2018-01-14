f9b0d2f9cef0ce1497462e65dd3d0d28e4b21aeb
Michele Bertoli <michele@berto.li>
2018-01-14 18:14:57 +0000

    stringify

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,13 @@ export const getComponentName = Component =>
 
 export const isStateless = Component =>
   !(Component.prototype && Component.prototype.isReactComponent)
+
+export const stringify = (state, path = []) => {
+  if (typeof state === 'string') {
+    return path.concat(state).join('.')
+  }
+  return Object.keys(state).reduce(
+    (prev, key) => prev.concat(stringify(state[key], path.concat(key))),
+    []
+  )
+}
