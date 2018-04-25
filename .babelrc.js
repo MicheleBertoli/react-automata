2cccece79e628906860628a8b177da6edba2b5ae
Mateusz Burzyński <mateuszburzynski@gmail.com>
2018-04-25 13:27:51 +0200

    Use loose mode for babel transform

--- .babelrc.js
+++ .babelrc.js
@@ -2,10 +2,12 @@ const { BABEL_ENV, NODE_ENV } = process.env
 const cjs = BABEL_ENV === 'cjs' || NODE_ENV === 'test'
 
 module.exports = {
-  presets: [['env', { modules: false }], 'react'],
+  presets: [['env', { modules: false, loose: true }], 'react'],
 
   plugins: [
     cjs && 'transform-es2015-modules-commonjs',
+    // TODO: after upgrading to babel@7:
+    // use `loose` for both class-properties & object-rest-spread
     'transform-class-properties',
     'transform-object-rest-spread',
     'babel-plugin-idx',
