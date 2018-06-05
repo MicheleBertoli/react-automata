719348b2651e6b5fc1cc6983d08d62d794015369
Michele Bertoli <MicheleBertoli@users.noreply.github.com>
2018-06-05 20:56:41 +0100

    support cond in tests (#54)

--- test/testStatechart.spec.js
+++ test/testStatechart.spec.js
@@ -1,67 +1,69 @@
 import React from 'react'
 import { Action, State, testStatechart, withStatechart } from '../src'
 
-const secondMachine = {
-  initial: 'a',
-  states: {
-    a: {
-      on: {
-        SECOND_NEXT: 'b',
+describe('conditional', () => {
+  const secondMachine = {
+    initial: 'a',
+    states: {
+      a: {
+        on: {
+          SECOND_NEXT: 'b',
+        },
+        onEntry: 'enterBA',
       },
-      onEntry: 'enterBA',
-    },
-    b: {
-      on: {
-        SECOND_NEXT: 'a',
+      b: {
+        on: {
+          SECOND_NEXT: 'a',
+        },
+        onEntry: 'enterBB',
       },
-      onEntry: 'enterBB',
     },
-  },
-}
+  }
 
-const firstMachine = {
-  initial: 'a',
-  states: {
-    a: {
-      on: {
-        FIRST_NEXT: 'b',
+  const firstMachine = {
+    initial: 'a',
+    states: {
+      a: {
+        on: {
+          FIRST_NEXT: 'b',
+        },
+        onEntry: 'enterA',
       },
-      onEntry: 'enterA',
-    },
-    b: {
-      on: {
-        FIRST_NEXT: 'a',
+      b: {
+        on: {
+          FIRST_NEXT: 'a',
+        },
+        onEntry: 'enterB',
+        ...secondMachine,
       },
-      onEntry: 'enterB',
-      ...secondMachine,
     },
-  },
-}
+  }
 
-test('action', () => {
-  const App = () => (
-    <div>
-      <Action show="enterA" hide="enterB">
-        a
-      </Action>
-      <Action show="enterBA">b.a</Action>
-      <Action show="enterBB">b.b</Action>
-    </div>
-  )
+  test('action', () => {
+    const App = () => (
+      <div>
+        <Action show="enterA" hide="enterB">
+          a
+        </Action>
+        <Action show="enterBA">b.a</Action>
+        <Action show="enterBB">b.b</Action>
+      </div>
+    )
 
-  testStatechart({ statechart: firstMachine }, App)
-})
+    testStatechart({ statechart: firstMachine }, App)
+  })
 
-test('state', () => {
-  const App = () => (
-    <div>
-      <State value="a">a</State>
-      <State value="b.a">b.a</State>
-      <State value="b.b">b.b</State>
-    </div>
-  )
+  test('state', () => {
+    const App = () => (
+      <div>
+        <State value="a">a</State>
+        <State value="b.a">b.a</State>
+        <State value="b.b">b.b</State>
+      </div>
+    )
 
-  testStatechart({ statechart: firstMachine }, App)
+    testStatechart({ statechart: firstMachine }, App)
+  })
 })
 
 test('parallel', () => {
@@ -170,4 +172,46 @@ test('channels', () => {
   )
 
   testStatechart({ statechart: outer, channel: 'outer' }, Outer)
+})
+
+describe('cond', () => {
+  const statechart = {
+    initial: 'a',
+    states: {
+      a: {
+        on: {
+          EVENT: {
+            b: {
+              cond: extState => extState.shouldPass,
+            },
+          },
+        },
+      },
+      b: {},
+    },
+  }
+
+  const Cond = () => (
+    <React.Fragment>
+      <State value="a">A</State>
+      <State value="b">B</State>
+    </React.Fragment>
+  )
+
+  test('pass', () => {
+    const extendedState = {
+      shouldPass: true,
+    }
+    const fixtures = {
+      a: {
+        EVENT: extendedState,
+      },
+    }
+
+    testStatechart({ statechart, fixtures, extendedState }, Cond)
+  })
+
+  test('fail', () => {
+    testStatechart({ statechart }, Cond)
+  })
 })
